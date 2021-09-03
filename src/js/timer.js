8a1513937442411e24c2a6829115aebe23cb8c19
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-09-03 11:53:08 +0300

    update timer.js event

--- src/js/timer.js
+++ src/js/timer.js
@@ -4,8 +4,9 @@ class CountdownTimer {
 
     static TIMER_ID_NUM = 1;
     
-    constructor(targetDate, parent) {
+    constructor(targetDate, eventType, parent) {
         this.id = `timer-${CountdownTimer.TIMER_ID_NUM}`;
+        this.eventType = eventType;
         this.targetDate = targetDate;
         this.parent = parent;
         CountdownTimer.TIMER_ID_NUM += 1;
@@ -15,19 +16,29 @@ class CountdownTimer {
         this.parent.insertAdjacentHTML(`${place}`, cb(this));
     }
     
+    findAllSpan() {
+        const daysSpan = document.querySelector('span[data-value="days"]');
+        const hoursSpan = document.querySelector('span[data-value="hours"]');
+        const minsSpan = document.querySelector('span[data-value="mins"]');
+        const secsSpan = document.querySelector('span[data-value="secs"]');
+
+        return { daysSpan, hoursSpan, minsSpan, secsSpan };
+    }
+
     timeData() {
         const intervalId = setInterval((() => {
             const timeDiff = new Date(this.targetDate) - Date.now();
+            const refs = this.findAllSpan();
 
             const days = String(Math.floor(timeDiff / (1000 * 60 * 60 * 24))).padStart(2, 0);
             const hours = String(Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, 0);
             const mins = String(Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, 0);
             const secs = String(Math.floor((timeDiff % (1000 * 60)) / 1000)).padStart(2, 0);
 
-            document.querySelector('span[data-value="days"]').textContent = days;
-            document.querySelector('span[data-value="hours"]').textContent = hours;
-            document.querySelector('span[data-value="mins"]').textContent = mins;
-            document.querySelector('span[data-value="secs"]').textContent = secs;
+            refs.daysSpan.textContent = days;
+            refs.hoursSpan.textContent = hours;
+            refs.minsSpan.textContent = mins;
+            refs.secsSpan.textContent = secs;
 
             if (days === '00' && hours === '00' && mins === '00' && secs === '00') {
                 clearInterval(intervalId);
@@ -41,7 +52,8 @@ class CountdownTimer {
 
 const parent = document.body;
 
-const newTimer = new CountdownTimer('2022/01/01', parent);
+const newTimer = new CountdownTimer('2022/01/01', 'New Year', parent);
+console.log(newTimer);
 
 newTimer.addTimerMarkup('afterbegin', timerMarkupTpl);
 
